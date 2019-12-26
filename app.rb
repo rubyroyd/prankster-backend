84e470f579efc4e409ae545527a90511ab6b4b72
ILYA Ramanenia <ruby.royd@gmail.com>
2019-12-27 00:55:37 +0300

    pretty JSON output

--- app.rb
+++ app.rb
@@ -1,23 +1,30 @@
 require "sinatra"
+require "json"
 require "./db/models.rb"
 
-get "/devices/:id" do
+get "/devices/:id.json" do
+  content_type :json
+
   device_id = params[:id]
   device = Device.find_by(device_id: device_id)
   if !device
     device = Device.build_fresh(device_id)
     device.save
   end
-  puts device.to_yaml
-  puts device.role
-
-  @display_devices = [device]
-  erb :devices
+  
+  json = device.as_json(:except => [ :id, :created_at])
+  json_string = JSON.pretty_generate(json)
+  puts json_string
+  json_string
 end
 
-get "/devices" do
-  @display_devices = Device.all
-  erb :devices
+get "/devices.json" do
+  content_type :json
+
+  json = Device.all.as_json(:except => [ :id, :created_at])
+  json_string = JSON.pretty_generate(json)
+  puts json_string
+  json_string
 end
 
 not_found do
