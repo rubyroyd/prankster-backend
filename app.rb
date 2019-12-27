cf6a3564cfdd46d7df41b671e1d40a6e335ae1fb
ILYA Ramanenia <ruby.royd@gmail.com>
2019-12-28 01:52:08 +0300

    API improvements

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "json"
 require "./db/models.rb"
 
-get "/devices/:id.json" do
+get "/devices/:id" do
   content_type :json
 
   device_id = params[:id]
@@ -15,16 +15,37 @@ get "/devices/:id.json" do
   json = device.as_json(:except => [ :id, :created_at])
   json_string = JSON.pretty_generate(json)
   puts json_string
+  status 200
   json_string
 end
 
-get "/devices.json" do
+get "/devices" do
   content_type :json
 
   json = Device.all.as_json(:except => [ :id, :created_at])
   json_string = JSON.pretty_generate(json)
   puts json_string
+  status 200
   json_string
+end
+
+post "/devices/:id" do
+  content_type :json
+
+  role = params[:role]
+
+  device_id = params[:id]
+  device = Device.find_by(device_id: device_id)
+  if !device
+    status 404
+    return "Device not found"
+  end
+
+  device.role = role
+  device.save
+
+  status 200
+  return "Assigned role " + device.role + " to device"
 end
 
 not_found do
